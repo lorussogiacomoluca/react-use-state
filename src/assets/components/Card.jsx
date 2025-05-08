da34f9d58bb53428930ef986361ce31be41d3463
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-08 20:10:56 +0200

    [BONUS] Show message "Nessun linguaggio selezionato" when no language is active

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Card = ({ language }) => {
-  if (!language) return null;
-
   return (
     <div className="card w-100">
       <div className="card-body">
-        <h5 className="card-title">{language.title}</h5>
-        <p className="card-text">{language.description}</p>
+        <h5 className="card-title">
+          {!language ? "Nessun linguaggio selezionato" : language.title}
+        </h5>
+        <p className="card-text">
+          {!language ? "Selezionare un linguaggio" : language.description}
+        </p>
       </div>
     </div>
   );
